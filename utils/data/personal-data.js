03433e90312f99dff16697ba2150df30f2728834
OnShubham <shubhamkharche3005@gmail.com>
2024-04-19 19:11:08 +0530

    add

---
+++ utils/data/personal-data.js
@@ -0,0 +1,19 @@
+export const personalData = {
+  name: "Shubham Kharche",
+  profile: "/profile.jpg",
+  designation: "Software Developer",
+  description:
+    "My name is Shubham Kharche. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "shubhamkharche3005@gmail.com",
+  phone: "+91 7350944278",
+  address: "Pune, Maharashtra, India",
+  github: "https://github.com/OnShubham",
+  facebook: "/",
+  linkedIn: "https://www.linkedin.com/in/shubhamkharche/",
+  twitter: "/",
+  stackOverflow: "/",
+  leetcode: "/",
+  devUsername: "/",
+  resume:
+    "https://drive.google.com/file/d/10UwUaOPS2XULQR7y32NvDV31sULayVYa/view?usp=sharing",
+};
